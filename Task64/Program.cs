a97169ee82599693a996c1b9a7244a8ae1588c91
AleksKolychev <aleksushome@gmail.com>
2023-02-25 09:53:46 +0500

    Add Task64

---
+++ Task64/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Задайте значение N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от N до 1. 
+Выполнить с помощью рекурсии */
+
+string rec(int n)
+{
+    if (n == 1)
+        return "1";
+    return $"{n}, " + rec(n - 1);
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(n));
